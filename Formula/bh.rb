3b44354b2a7831572a70ee88222423655d5ca6e6
senkentarou <senkentarou@gmail.com>
2026-08-28 14:49:15 +0900

    bh 0.6.0: drop redundant version field

--- Formula/bh.rb
+++ Formula/bh.rb
@@ -2,7 +2,6 @@ class Bh < Formula
   desc "Fast, interactive bash history search with fuzzy matching and smart ranking"
   homepage "https://github.com/senkentarou/bh"
   url "https://github.com/senkentarou/bh/releases/download/v0.6.0/bh-aarch64-apple-darwin.tar.gz"
-  version "0.6.0"
   sha256 "93f8b95e9df431e24a1c72e61423d7e7b06e27572d1a1da3755ab7c6817c32d6"
   license "MIT"
 
